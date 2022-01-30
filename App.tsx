c8bfda994141c52bc2827ab64dad8d28d6b01e5e
sourcetreeeeeeeeeeeeeee <sourcetreeeeeeeeeeeeeee@gmail.com>
2022-01-30 13:07:53 +0800

    now works in ios, but android will flash back

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import App2 from './App2';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <App2 />
+    // <View style={styles.container}>
+    //   <Text>Open up App.tsx to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
---
+++ screens/ExploreScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+const ExploreScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>ExploreScreen</Text>
+      <Button
+        title="Click Here"
+        onPress={() => alert("Button Clicked!")}
+      />
+    </View>
+  );
+};
+
+export default ExploreScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
